47a95f855b507e8e96cd1dcc21ee43a6c2e4ee5f
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2023-05-25 21:27:04 +1000

    added effects for all quadrants

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,8 @@
 let snowContainer;
-const snowContent = ['&#10052', '&#10053', '&#10054'];
-const autumnLeavesContent = ['&#127809;', '&#127810;'];
+const snowContent = ['\u2744', '\u2745', '\u2746'];
+const autumnLeavesContent = ['./assets/sprites/maple_sprite.png', './assets/sprites/fallen_leaf_sprite.png'];
+const summerLeavesContent = ['./assets/sprites/green_falling_leaf_sprite.png', './assets/sprites/herb_sprite.png', './assets/sprites/flower_sprite.png', './assets/sprites/oak_leaf_sprite.png'];
+const cherryBlossomsContent = ['./assets/sprites/cherry_blossom_1_sprite.png', './assets/sprites/cherry_blossom_2_sprite.png', './assets/sprites/cherry_blossom_3_sprite.png'];
 
 const random = (num) => {
   return Math.floor(Math.random() * num);
@@ -29,25 +31,6 @@ const createSnow = (num) => {
   }
 }
 
-function createAutumnLeaves(num) {
-    for (let i = num; i > 0; i--) {
-        var aLeaf = document.createElement("div");
-        aLeaf.className = "aLeaf effectElement";
-        aLeaf.style.cssText = getRandomStyles();
-        aLeaf.innerHTML = autumnLeavesContent[random(autumnLeavesContent.length)];
-        $('#autumnleavescontainer').append(aLeaf);
-    }
-}
-
-function removeAutumnLeaves() {
-    setTimeout(() => {
-        const aLeaves = document.querySelectorAll('.aLeaf');
-        for (const aL of aLeaves) {
-            aL.remove();
-        }
-    }, 200);
-}
-
 const removeSnow = () => {
   setTimeout(() => {
     const snow = document.querySelectorAll('.snow');
@@ -57,16 +40,86 @@ const removeSnow = () => {
   }, 200);
 }
 
-window.addEventListener("load", () => {
-    $('#loadwrapper').fadeOut(2500, () => {
-        $('#loadwrapper').remove();
-    });
-    snowContainer = document.getElementById("snowcontainer");
-    snowEvents();
-    autumnLeavesEvents();
+function createAutumnLeaves(num) {
+  for (let i = num; i > 0; i--) {
+      var aLeaf = document.createElement("div");
+      aLeaf.className = "aLeaf effectElement";
+      let initialStyle = getRandomStyles();
+      let dim = random(25) + 20;
+      let bg = autumnLeavesContent[random(autumnLeavesContent.length)];
 
+      aLeaf.style.cssText = `${initialStyle} width: ${dim}px; height: ${dim}px; background-image: url(${bg});`;
+      $('#autumnleavescontainer').append(aLeaf);
+  }
+  twemoji.parse(document.body);
+}
+
+function removeAutumnLeaves() {
+  setTimeout(() => {
+      const aLeaves = document.querySelectorAll('.aLeaf');
+      for (const aL of aLeaves) {
+          aL.remove();
+      }
+  }, 200);
+}
+
+function createSummerLeaves(num) {
+  for (let i = num; i > 0; i--) {
+    let sLeaf = document.createElement("div");
+    sLeaf.className = "sLeaf effectElement";
+    let initialStyle = getRandomStyles();
+    let dim = random(25) + 15;
+    let bg = summerLeavesContent[random(summerLeavesContent.length)];
+
+    sLeaf.style.cssText = `${initialStyle} width: ${dim}px; height: ${dim}px; background-image: url(${bg});`;
+    $('#summerleavescontainer').append(sLeaf);
+  }
+} 
+
+function removeSummerLeaves() {
+  setTimeout(() => {
+    const sLeaves = document.querySelectorAll('.sLeaf');
+    for (const sL of sLeaves) {
+        sL.remove();
+    }
+  }, 200);
+}
+
+function createBlossoms(num) {
+  for (let i = num; i > 0; i--) {
+    let cBlossom = document.createElement("div");
+    cBlossom.className = "cBlossom effectElement";
+    let initialStyle = getRandomStyles();
+    let dim = random(25) + 10;
+    let bg = cherryBlossomsContent[random(cherryBlossomsContent.length)];
+
+    cBlossom.style.cssText = `${initialStyle} width: ${dim}px; height: ${dim}px; background-image: url(${bg});`;
+    $('#cherryblossomscontainer').append(cBlossom);
+  }
+}
+
+function removeBlossoms() {
+  setTimeout(() => {
+    const cBlossoms = document.querySelectorAll('.cBlossom');
+    for (const cB of cBlossoms) {
+        cB.remove();
+    }
+  }, 200);
+}
+
+window.addEventListener("load", () => {
+  $('#loadwrapper').fadeOut(2500, () => {
+      $('#loadwrapper').remove();
+  });
+  snowContainer = document.getElementById("snowcontainer");
+  snowEvents();
+  autumnLeavesEvents();
+  summerLeavesEvents();
+  cherryBlossomsEvents();
 });
 
+
+///////////////
 
 function snowEvents() {
     const quad4 = document.getElementById("quad4");
@@ -91,4 +144,28 @@ function autumnLeavesEvents() {
     quad3.addEventListener('mouseleave', () => {
         removeAutumnLeaves();
     });
+}
+
+function summerLeavesEvents() {
+  const quad2 = document.getElementById("quad2");
+
+  quad2.addEventListener('mouseenter', () => {
+    createSummerLeaves(40);
+  });
+
+  quad2.addEventListener('mouseleave', () => {
+    removeSummerLeaves();
+  });
+}
+
+function cherryBlossomsEvents() {
+  const quad1 = document.getElementById("quad1");
+
+  quad1.addEventListener('mouseenter', () => {
+    createBlossoms(40);
+  });
+
+  quad1.addEventListener('mouseleave', () => {
+    removeBlossoms();
+  });
 }
